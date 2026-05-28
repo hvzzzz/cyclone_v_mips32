86be3151737ad9cf219c23c03de3ad869f731fa5
hvzzzz <hananquispec@outlook.com>
2026-05-29 00:59:14 +0200

    final presentation ready

--- scripts/fibo.asm
+++ scripts/fibo.asm
@@ -1,16 +1,16 @@
-      add  $1, $0, $0   # $1 = 0  (Current Fib)
-      addi $2, $0, 1    # $2 = 1  (Next Fib)
-      add  $3, $0, $0   # $3 = 0  (Memory Address Pointer)
-      addi $6, $0, 0    # $6 = 0  (Initialization of the counter)
-      addi $4, $0, 49   # (Loop Limit)
+      add  $1, $0, $0   # $1 = 0 -> F(0) = 0 (Current Fib)
+      addi $2, $0, 1    # $2 = 1 -> F(1) = 1 (Next Fib)
+      add  $3, $0, $0   # $3 = 0 -> (Memory Address Pointer)
+      addi $6, $0, 0    # $6 = 0 -> (Initialization of the counter)
+      addi $4, $0, 49   # $4 = 49 -> (Loop Limit)
 
-loop: beq  $6, $4, 7    # If Pointer == $4, jump forward 7 instructions to 'exit'
-      sw   $1, 0($3)    # Store Current Fib to Data Memory
-      add  $5, $1, $2   # Temp = Current + Next
-      add  $1, $0, $2   # Current = Next
-      add  $2, $0, $5   # Next = Temp
-      addi $3, $3, 4    # Pointer = Pointer + 4
-      addi $6, $6, 1    # Counter = Counter +1
-      beq  $0, $0, -8   # Unconditional jump backward 8 instructions to 'loop'
+loop: beq  $6, $4, 7    # $6 == $4? -> If Pointer == $4, jump forward 7 instructions to 'exit'
+      sw   $1, 0($3)    # $1 = 0 -> Store Current Fib to Data Memory
+      add  $5, $1, $2   # $5 = $1 + $2 # 0 + 1 -> F(2) = F(1) + F(0) # F(n) = F(n-1)+F(n-2)
+      add  $1, $0, $2   # $1 = $0 + $2 # 0 + 1 -> F(0) = F(1) # F(n-2) = F(n-1)
+      add  $2, $0, $5   # $2 = $0 + $5 # 0 + 1 -> F(1) = F(2) # F(n-1) = F(n)
+      addi $3, $3, 4    # $3 = $3 + 4 -> Pointer = Pointer + 4
+      addi $6, $6, 1    # $6 = $6 + 1 -> Counter = Counter +1
+      beq  $0, $0, -8   # $0 == $0? -> Unconditional jump backward 8 instructions to 'loop'
 
 exit: beq  $0, $0, -1   # Infinite trap
--- scripts/isa.asm
+++ scripts/isa.asm
@@ -1,24 +1,25 @@
-    add  $0, $0, $0 # zero value
-    add  $4, $0, $0 # memory pointer
+    add  $0, $0, $0 # $0 = 0 -> zero value
+    add  $4, $0, $0 # $4 = 0 -> memory pointer
 
-    addi $1, $0, 5  # store 5 in register 1
-    addi $2, $0, 60 # store 60 in register 2
-    add $3, $1, $2  # register 3 = $1+$2
-    sub $5, $2, $1  # register 5 = $2-$1
-    sw $3, 0($4)    # save register 3 in data memory
-    addi $4, $4, 4
-    sw $5, 0($4)    # save register 5 in data memory
+    addi $1, $0, 5  # $1 = $0 + 5 -> store 5 in register 1
+    addi $2, $0, 60 # $2 = $0 + 60 -> store 60 in register 2
+    add $3, $1, $2  # $3 = $1 + $2  = 65 -> register 3 = $1+$2
+    sub $5, $2, $1  # $5 = $2 - $1  = 55 -> register 5 = $2-$1
+    sw $3, 0($4)    # save register 3 in data memory at position 0
+    addi $4, $4, 4  # $4 = 4 -> update memory pointer
+    sw $5, 0($4)    # save register 5 in data memory at position 4
 
-    slt $7, $3, $5
-    slt $8, $5, $3
-    sw $7, 0($4)
-    addi $4, $4, 4
-    sw $8, 0($4)
-    addi $4, $4, 4
-    addi $4,$4, -12
+    slt $7, $3, $5  # $7 = 0 because $3 < $5 = false / 65 < 55
+    slt $8, $5, $3  # $8 = 1 because $5 < $3 = true / 55 < 65
+    addi $4, $4, 4  # $4 = 8 -> update memory pointer
+    sw $7, 0($4)    # save register 7 in data memory at position 8
+    addi $4, $4, 4  # $4 = 12 -> update memory pointer
+    sw $8, 0($4)    # save register 8 in data memory at position 12
+    addi $4, $4, 4  # $4 = 16 -> update memory pointer
+    addi $4,$4, -16 # $4 = 0 -> update memory pointer
 
-    lw $6, 0($4)    # read from data memory to register 6
-    addi $6,$6,30   # add 30 to saved data
+    lw $6, 0($4)    # $6 = 65 -> read from data memory to register 6
+    addi $6,$6,30   # $6 = 65 + 30 -> add 30 to saved data
     sw $6, 0($4)    # store result
 
     beq  $0, $0, -1 # end
